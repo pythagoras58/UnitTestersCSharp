f9ba8b6056e317beb7f77c2dfa2bdb8b3d71ac7d
pythagoras58 <solomondanso58@gmail.com>
2023-02-07 22:04:01 +0000

    Initial commit

---
+++ BasicUnitTesters/DumbestFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicUnitTesters;
+
+public class DumbestFunction
+{
+    public string ReturnsBabes(int num)
+    {
+        if (num == 0)
+        {
+            return "Next Time";
+        }
+        else
+        {
+            
+            return "Love you bae";
+        }
+    }
+}
---
+++ BasicUnitTesters/Tests/DumbestFunctionTest.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicUnitTesters.Tests;
+
+public static class DumbestFunctionTest
+{
+    public static void DumbestFunction_ReturnsBabes_ReturnString()
+    {
+        try
+        {
+            // Arrange - Get variables, classes and functions
+            
+            //Act
+            
+            //Assert
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
