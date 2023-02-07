6d09db5f06b19d0711c5db327090090c700d5a65
pythagoras58 <solomondanso58@gmail.com>
2023-02-07 23:07:02 +0000

    Tester -3

---
+++ FakeNetworks.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FakeNetworks.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
